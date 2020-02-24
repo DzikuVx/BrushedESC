73988189c2da63cc0c9a47bc54c9a6e5ae37627c
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2020-02-24 14:12:29 +0100

    Reversible motors handling

--- brushed_esc.ino
+++ brushed_esc.ino
@@ -8,6 +8,16 @@
 volatile unsigned long risingStart = 0;
 volatile long channelLength = 0;
 
+#define PWM_LENGTH_MIN 500
+#define PWM_LENGTH_MAX 2500
+
+#define PWM_INPUT_MIN 1000
+#define PWM_INPUT_MAX 2000
+#define PWM_INPUT_NEUTRAL 1460
+#define PWM_FORWARD_THRESHOLD 1490
+#define PWM_BACKWARD_THRESHOLD 1430
+#define INITIAL_POWER_LEVEL 24
+
 void setup()
 {
     Serial.begin(115200);
@@ -38,21 +48,57 @@ void onRising0(void)
 }
 
 long getInput() {
-    if (micros() - risingStart > 1000000L || risingStart == 0) {
+    if (micros() - risingStart > 25000 || risingStart == 0) {
         return 0;
     } else {
         return channelLength;
     }
 }
 
+void motorStop(void) {
+    analogWrite(PWM_PIN, 0);
+}
+
 void loop()
 {
 
-    if (getInput() > 1100 && getInput() < 2200) {
+    const int input = getInput();
+
+    if (input > PWM_LENGTH_MIN && input < PWM_LENGTH_MAX) {
         //We have the input, scale the power between 25 and 100%
-        analogWrite(PWM_PIN, constrain(map(getInput(), 1100, 1850, 32, 255), 0, 255));
+        // analogWrite(PWM_PIN, constrain(map(getInput(), 1100, 1850, 32, 255), 0, 255));
+
+        const int inputConstrained = constrain(input, PWM_INPUT_MIN, PWM_INPUT_MAX);
+        int outputValue = 0;
+
+        if (inputConstrained > PWM_FORWARD_THRESHOLD) {
+            /*
+             * Forward direction
+             */
+            outputValue = map(inputConstrained, PWM_FORWARD_THRESHOLD, PWM_INPUT_MAX, INITIAL_POWER_LEVEL, 255);
+
+            digitalWrite(DIRECTION_PIN, LOW);
+        } else if (inputConstrained < PWM_BACKWARD_THRESHOLD) {
+            /*
+             * Backward direction
+             * Since in reverse mode DIRECTION_PIN is pulled up, PWM value on the output has to be reversed: 0 means full duty cycle
+             */
+            outputValue = map(inputConstrained, PWM_INPUT_MIN, PWM_BACKWARD_THRESHOLD, 0, 255 - INITIAL_POWER_LEVEL);
+
+            digitalWrite(DIRECTION_PIN, HIGH);
+        } else {
+            /*
+             * In deadband
+             */
+            outputValue = 0;
+            digitalWrite(DIRECTION_PIN, LOW);
+        }
+
+        analogWrite(PWM_PIN, constrain(outputValue, 0, 255));
     } else {
         // No input, stop the motors
-        analogWrite(PWM_PIN, 0);
+        motorStop();
     }
+
+    delay(10);
 }
